89d25b70bfdf21e8f8bc690013a2e42a0f4d1c5d
lirundong <lirundong@sensetime.com>
2019-04-25 21:09:07 +0800

    HW5/6 init release

---
+++ include/DownhillSimplex.hpp
@@ -0,0 +1,39 @@
+#ifndef CS133_HW56_DHSIMPLEX_HPP_
+#define CS133_HW56_DHSIMPLEX_HPP_
+
+#include <stdlib.h>
+
+
+template< typename OBJECTIVEFUNCTION >
+class DownhillSimplex {
+ public:
+  typedef OBJECTIVEFUNCTION ObjectiveFunction;
+  typedef typename ObjectiveFunction::Scalar Scalar;
+  typedef typename ObjectiveFunction::InputType InputType;
+  typedef typename ObjectiveFunction::ValueType ValueType;
+  typedef typename ObjectiveFunction::JacobianType JacobianType;
+  
+  DownhillSimplex(
+      ObjectiveFunction & objectiveFunction,
+      Scalar xtol = 0.0001,
+      Scalar ftol = 0.0001,
+      size_t maxIterations = 100 );
+  virtual ~DownhillSimplex();
+
+  //return value: -1 if optimization finished prematurely (stopped because maxiterations was reached)
+  //               1 if finished because of norm(step) < xtol
+  //               2 if finished because of norm(energyBefore-energyAfter) < ftol
+  int minimize( InputType & input );
+
+private:
+  ObjectiveFunction & _objectiveFunction;
+
+  Scalar _xtol;
+  Scalar _ftol;
+  size_t _maxIterations;
+  size_t _iterations;
+};
+
+#include "DownhillSimplex_impl.hpp"
+
+#endif
